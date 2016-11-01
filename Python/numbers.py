9d8eae496532f5bdaf0cd0345f201bd92b247cc3
Hanlei Qin <qinhanlei@gmail.com>
2016-11-02 00:01:26 +0800

    fixed.

--- Python/numbers.py
+++ Python/numbers.py
@@ -23,9 +23,11 @@ def _check_python_version():
 
 
 def is_prime(n):
+    if n < 2:
+        return False
     if n == 2:
         return True
-    if (n % 2 == 0) or (n == 1):
+    if n % 2 == 0:
         return False
     k = (int)(math.sqrt(n)) + 1
     for i in range(3, k + 1, 2):
